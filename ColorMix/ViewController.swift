0e286d26a03152467d9439b57af45e3bf1a82c18
Fernando <fernandoebarraza@gmail.com>
2018-04-04 21:57:35 -0300

    Agrego switch de control de color

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        colorView.backgroundColor = .black
+        updateColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +22,35 @@ class ViewController: UIViewController {
     }
 
     @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    
+    @IBOutlet weak var blueSwitch: UISwitch!
+    
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+       updateColor()
+    }
+    
+    func updateColor(){
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if redSwitch.isOn{
+            red = 1
+        }
+        if greenSwitch.isOn{
+            green = 1
+        }
+        if blueSwitch.isOn{
+            blue = 1
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
+    
     
 }
 
